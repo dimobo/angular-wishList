089591e8d0777aee9c565cd0cb07d624b3688b4e
Montero Borras <dmontero@t-systems.es>
2021-10-28 15:37:12 +0200

    delete saludador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SaludadorComponent } from './saludador/saludador.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SaludadorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
