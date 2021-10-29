183ec5d9cfb17411e2eb950c9813e8717f40fe47
Montero Borras <dmontero@t-systems.es>
2021-10-29 13:38:59 +0200

    Complete Semana 1

--- src/app/lista-destinos/lista-destinos.component.ts
+++ src/app/lista-destinos/lista-destinos.component.ts
@@ -13,6 +13,16 @@ export class ListaDestinosComponent implements OnInit {
   }
 
   guardar(n: string, d: string, u: string): boolean {
+
+    // Con este if tratamos si la publicación en el tiene texto en el campo URL
+    // Pero por ahora no esta discriminando si el texto es una imagen o no
+    // Por lo que si escribimos texto que no es una URL a una imagen no se mostrara nada
+    // Pero si escribimos una URL valida de una imagen si que la mostrara i en caso de que
+    // No introduzcamos nada por defecto se quedara la imagen del placeholder.
+    if (u == '') {
+      u = 'https://placeimg.com/380/230/nature';
+    }
+
     this.destinos.push(new DestinoViaje(n, d, u));
     return false;
   }
